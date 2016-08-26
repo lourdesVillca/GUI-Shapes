39b099434b346c95aaf5f3d779164f21842f915d
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-08-26 15:50:15 -0400

    refactor code to code review

---
+++ src/main/java/org.fundacionjala.org/Shape.java
@@ -0,0 +1,22 @@
+package org.fundacionjala.org;
+
+/**
+ * Base class to calculate Area and Perimeter.
+ */
+public interface Shape {
+
+    /**
+     * Calculates the Area.
+     *
+     * @return the calculated Area.
+     */
+    double calculateArea();
+
+    /**
+     * Calculates the perimeter.
+     *
+     * @return the calculated perimeter.
+     */
+    double calculatePerimeter();
+
+}
---
+++ src/main/java/org.fundacionjala.org/Square.java
@@ -0,0 +1,17 @@
+package org.fundacionjala.org;
+
+/**
+ * Shape that inherit the methods of Square.
+ */
+public class Square extends Rectangle {
+
+    /**
+     * Constructor.
+     *
+     * @param side of the Square
+     */
+    public Square(int side) {
+        super(side, side);
+    }
+
+}
